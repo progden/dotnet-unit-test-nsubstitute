8bc7964647a1444ca837282d204da83ee84cce92
Dennis.Chang <dennis.chang@hy-tech.com.tw>
2023-02-09 22:08:36 +0800

    [feature] 呼叫 ph

---
+++ SaleLib/SaleService.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace SaleLib;
+
+public class SaleService
+{
+    private readonly PhoneCheckService _phoneCheckService;
+
+    public SaleService(PhoneCheckService checkService)
+    {
+        this._phoneCheckService = checkService;
+    }
+
+    public ApplyNewContractResponse ApplyNewContract(User user)
+    {
+        if (user.Age < 18)
+            return new ("fail");
+        var rs = _phoneCheckService.Check(user.PhoneNum);
+
+        return rs.IsUsed ? new("fail") : new("success");
+    }
+}
